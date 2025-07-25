cfbbcf204cc63b2fc2fc65ea747160e9de90ced3
Manasa Duvvuri <adityakon@outlook.com>
2025-07-25 16:46:57 -0400

    browser-handling

--- e2e/testinvalid.spec.js
+++ e2e/testinvalid.spec.js
@@ -1,4 +1,4 @@
-const { test } = require('@playwright/test')
+const { test, expect } = require('@playwright/test')
 
 test('Invalid login error message', async ({ browser }) => {
 	const context = await browser.newContext();
@@ -39,7 +39,7 @@ test('change from admin to user', async ({ page }) => {
 	await page.locator('.radiotextsty').last().click()
 	await page.locator('#okayBtn').click()
 	await page.pause()
-	
+
 })
 
 test('admin to user', async ({ page }) => {
@@ -51,3 +51,49 @@ test('admin to user', async ({ page }) => {
 	await page.pause()
 
 })
+
+test('first assertion using expect', async ({ page }) => {
+	await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+	await page.locator('#username').fill("rahulshettyacademy");
+	await page.locator('#password').fill("learning");
+	await page.locator('.radiotextsty').last().click()
+	await expect(page.locator('.radiotextsty').last()).toBeChecked()
+	await page.locator('#okayBtn').click()
+
+
+})
+
+test('to check blinking text', async ({ page }) => {
+	await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+	await expect(page.locator('.blinkingText')).toHaveAttribute('class', 'blinkingText')
+})
+
+
+test.only('checking new page handling', async ({ browser }) => {
+	const context = await browser.newContext()
+	const page = await context.newPage()
+	await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+	console.log(await page.locator('.blinkingText').textContent())
+	//const [page2] = await Promise.all([
+	//	page.locator('.blinkingText').click(),
+	//	context.waitForEvent('page')
+	//])
+
+	//await page2.locator('[href="mailto:mentor@rahulshettyacademy.com"]').click()
+	//console.log(await page2.locator('.inner-box').textContent())
+
+	const [newPage] = await Promise.all(
+		[
+			context.waitForEvent('page'),//listen for any new page pending,rejected,fulfilled
+			page.locator('.blinkingText').click(),
+
+		])//new page is opened
+
+
+	const text = await newPage.locator(".red").textContent();
+	console.log(text)
+
+	await newPage.locator('[href="mailto:mentor@rahulshettyacademy.com"]').click()
+
+})
+
